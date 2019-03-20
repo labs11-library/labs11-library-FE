4b95bc5b39a77f43dcb817b6ea8a7571a294f245
Blake Fletcher <blkfltchr@gmail.com>
2019-03-20 18:01:28 -0400

    Create react app installed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <p>
+            Book maps 📚🗺
+          </p>
+      </div>
+    );
+  }
+}
+
+export default App;
