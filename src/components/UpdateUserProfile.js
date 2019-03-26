a24bb89c9d041aeb9f6303ff274724e3a71f3e8b
Zooheck <davidscottflack@gmail.com>
2019-03-25 20:16:08 -0500

    Populate user profile update form

--- src/components/UpdateUserProfile.js
+++ src/components/UpdateUserProfile.js
@@ -1,7 +1,52 @@
 import React, { Component } from "react";
 
-export default class UpdateUserProfile extends Component {
+class UpdateUserProfile extends Component {
+  constructor(props) {
+    super(props);
+    const { firstName, lastName, email, bio } = this.props.loggedInUser;
+    this.state = {
+      firstName,
+      lastName,
+      email,
+      bio
+    };
+  }
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
   render() {
-    return <div />;
+    return (
+      <div>
+        <input
+          type="text"
+          name="firstName"
+          value={this.state.firstName}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="lastName"
+          value={this.state.lastName}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="email"
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="bio"
+          value={this.state.bio}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
   }
 }
+
+export default UpdateUserProfile;
