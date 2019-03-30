494c41cd3e95240a07eef7c1e0901034e74ad336
Blake Fletcher <blkfltchr@gmail.com>
2019-03-30 18:18:13 -0400

    SingleCheckedOutBook renders at /users/:userId/checkedOut/:checkedOutId

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,10 +3,12 @@ import authReducer from "./authReducers.js";
 import bookReducer from "./bookReducers.js";
 import inventoryReducer from "./inventoryReducers.js";
 import userReducer from "./userReducers.js";
+import reviewReducer from "./reviewReducers.js";
 
 export default combineReducers({
   authReducer,
   bookReducer,
   inventoryReducer,
-  userReducer
+  userReducer,
+  reviewReducer
 });
