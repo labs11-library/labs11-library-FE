f5b1cd93f4540405f13fb4848faadb3fc09f707f
Zooheck <davidscottflack@gmail.com>
2019-04-02 16:01:55 -0500

    Implement add checkout request functionality

--- src/redux/actions/checkoutActions.js
+++ src/redux/actions/checkoutActions.js
@@ -4,6 +4,9 @@ import baseUrl from "../../url";
 export const GETTING_CHECKOUT_REQUESTS = "GETTING_CHECKOUT_REQUESTS";
 export const GET_CHECKOUT_REQUESTS_SUCCESS = "GET_CHECKOUT_REQUESTS_SUCCESS";
 
+export const ADDING_CHECKOUT_REQUEST = "ADDING_CHECKOUT_REQUEST";
+export const ADD_CHECKOUT_REQUEST_SUCCESS = "ADD_CHECKOUT_REQUEST_SUCCESS";
+
 export const getCheckoutRequests = () => dispatch => {
   const userId = localStorage.getItem("userId");
   dispatch({ type: GETTING_CHECKOUT_REQUESTS });
@@ -13,6 +16,24 @@ export const getCheckoutRequests = () => dispatch => {
       dispatch({
         type: GET_CHECKOUT_REQUESTS_SUCCESS,
         payload: res.data
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+export const addCheckoutRequest = (bookId, lenderId) => dispatch => {
+  const userId = localStorage.getItem("userId");
+  dispatch({ type: ADDING_CHECKOUT_REQUEST });
+  axios
+    .post(`${baseUrl}/users/${userId}/checkoutRequest`, {
+      bookId,
+      lenderId
+    })
+    .then(res => {
+      dispatch({
+        type: ADD_CHECKOUT_REQUEST_SUCCESS
       });
     })
     .catch(err => {
--- src/redux/reducers/checkoutReducers.js
+++ src/redux/reducers/checkoutReducers.js
@@ -1,6 +1,8 @@
 import {
   GETTING_CHECKOUT_REQUESTS,
-  GET_CHECKOUT_REQUESTS_SUCCESS
+  GET_CHECKOUT_REQUESTS_SUCCESS,
+  ADDING_CHECKOUT_REQUEST,
+  ADD_CHECKOUT_REQUEST_SUCCESS
 } from "../actions/checkoutActions.js";
 
 const initialState = {
@@ -21,6 +23,16 @@ export default function checkoutReducer(state = initialState, action) {
         ...state,
         loadingRequests: false,
         checkoutRequests: action.payload
+      };
+    case ADDING_CHECKOUT_REQUEST:
+      return {
+        ...state,
+        loadingRequests: true
+      };
+    case ADD_CHECKOUT_REQUEST_SUCCESS:
+      return {
+        ...state,
+        loadingRequests: false
       };
     default:
       return state;
