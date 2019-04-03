6c23b9004bc3e7bf2feaaf859b8f08baf9e56c59
Blake Fletcher <blkfltchr@gmail.com>
2019-04-03 18:42:32 -0400

    SingleCheckOut renders chatapp

--- src/components/CheckedOut/SingleCheckedOutBook.js
+++ src/components/CheckedOut/SingleCheckedOutBook.js
@@ -1,107 +1,67 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 import * as moment from "moment";
 import "@progress/kendo-theme-material/dist/all.css";
-import { Button } from "@progress/kendo-react-buttons";
 import { connect } from "react-redux";
-import { getSingleCheckedOutBook } from "../../redux/actions/bookActions.js";
+import { getSingleCheckout } from "../../redux/actions/checkoutActions.js";
 import { getLoggedInUser } from "../../redux/actions/authActions.js";
-import ReviewForm from "../Reviews/ReviewForm";
 import ChatApp from "../Chat/ChatApp";
 
-const BookDetailsWrapper = styled.div`
-  width: 60vw;
-  border-bottom: 2px solid grey;
-  display: flex;
-  justify-content: space-between;
-  margin: 20px auto;
-  height: 400px;
-`;
-const BookImgWrapper = styled.div`
-  width: 250px;
-  height: 375px;
-`;
-const BookImg = styled.img`
-  width: 100%;
-  height: 100%;
-`;
-
-const Availability = styled.p`
-  color: ${props => (props.available ? "green" : "red")};
-`;
-
 class SingleCheckedOutBook extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      singleCheckedOutBook: {},
-      showChat: false
-    };
-  }
 
   componentDidMount() {
-    this.props.getSingleCheckedOutBook(this.props.match.params.checkedOutId);
+    const userId = localStorage.getItem('userId')
+    this.props.getSingleCheckout(userId, this.props.match.params.checkoutId);
     this.props.getLoggedInUser();
+  }
+
+  timeRemaining = (dueDate) => {
+    let now = moment(Date.now());
+    let end = moment(dueDate);
+    let duration = moment.duration(now.diff(end)).humanize();
+    return duration;
   }
 
   render() {
     console.log(this.props);
     console.log(this.state);
-    if (!this.props.singleCheckedOutBook) {
-      return <h1>Loading...</h1>;
-    } else if (this.props.singleCheckedOutBook && !this.state.showChat) {
-      const {
+    // if (this.props.loadingUser) {
+    //   return <h1>Loading...</h1>;
+    // } 
+    const {
         title,
         authors,
-        image,
         lender,
-        checkedOutId,
-        returned,
-        available,
-        checkoutDate
-      } = this.props.singleCheckedOutBook;
-      const availability = available ? "Available" : "Checked out";
-      const threeWeeks = moment(checkoutDate, "YYYY-MM-DD").add(21, "days");
-      const dueDate = moment
-        .utc(threeWeeks)
-        .local()
-        .format("dddd, MMMM Do");
+        dueDate,
+        lenderId,
+        borrower,
+        borrowerId
+      } = this.props.singleCheckout;
+    
+      const dateDue = moment
+            .utc(dueDate)
+            .local()
+            .format("dddd, MMMM Do");
+      const lenderBorrowerName = this.props.singleCheckout.lenderId === localStorage.getItem("userId") ? borrower : lender
+      const otherUserId = lenderId === localStorage.getItem("userId") ? borrowerId : lenderId
+      console.log("lenderId", lenderId)
       return (
         <div>
-          <BookDetailsWrapper>
-            <BookImgWrapper>
-              <BookImg alt={title} src={image} />
-            </BookImgWrapper>
-            <div>
-              <h2>{title}</h2>
-              <p>by {authors}</p>
-              <Availability available={available}>{availability}</Availability>
-              {!available && <p>Date due: {dueDate}</p>}{" "}
-              {/*{timeRemaining(dueDate)}*/}
-              <p>Contact {lender} from around the way</p>
-              <Button onClick={() => this.setState({showChat: true})}>Send message</Button>
-              {returned === 0 && <ReviewForm reviewEvent={checkedOutId} />}
-            </div>
-          </BookDetailsWrapper>
+              <h2>Talk to {lenderBorrowerName} about returning {title} by {authors}</h2>
+              <p>Due: {dateDue} ({this.timeRemaining(dueDate)} from now)</p>
+              <ChatApp user={this.props.loggedInUser} otherUserId={otherUserId}/>
         </div>
       );
-    } else if (this.state.showChat) {
-      return (
-        <ChatApp user={this.props.loggedInUser} otherUserId={this.props.singleCheckedOutBook.lenderId}/>
-      )
-    }
+    } 
   }
-}
 
-const mapStateToProps = state => {
-  return {
-    loading: state.bookReducer.fetchingBooks,
-    singleCheckedOutBook: state.bookReducer.singleCheckedOutBook,
-    loggedInUser: state.authReducer.loggedInUser
-  };
-};
+const mapStateToProps = state => ({
+  loadingCheckouts: state.checkoutReducer.loadingCheckouts,
+  loadingUser: state.authReducer.fetchingUser,
+  singleCheckout: state.checkoutReducer.singleCheckout,
+  loggedInUser: state.authReducer.loggedInUser
+});
 
 export default connect(
   mapStateToProps,
-  { getSingleCheckedOutBook, getLoggedInUser }
+  { getSingleCheckout, getLoggedInUser }
 )(SingleCheckedOutBook);
