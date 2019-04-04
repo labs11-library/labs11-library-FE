611bb3c923a500426450772f711418f79cabd8b8
Blake Fletcher <blkfltchr@gmail.com>
2019-04-04 14:06:12 -0400

    LandingPage responsive

--- src/components/Layout/LandingPage.js
+++ src/components/Layout/LandingPage.js
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import BookList from '../Books/BookList';
 import Mapview from '../Map/Mapview';
+import { LandingPageWrapper, TabsWrapper, BooksWrapper, MapWrapper } from './styled.js';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-const LandingPage = () => {
-    return ( 
-        <div>
-            <div style={{textAlign: "center", textTransform: "uppercase", fontWeight: "bold", fontSize: "48px", margin: "20px"}}>Book maps{" "}
-                <span role="img" aria-label="books">
-                    📚
-                </span>
-                <span role="img" aria-label="map">
-                    🗺
-                </span>
+class LandingPage extends Component {
+    state = { 
+        value: 0
+     }
+    handleChange = (event, value) => {
+        this.setState({ value });
+      };
+    
+    render() {
+        console.log("this.state.value", this.state.value)
+        return ( 
+            <div>
+                <LandingPageWrapper>
+                <TabsWrapper>
+                    <Paper>
+                        <Tabs
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        centered
+                        variant="fullWidth"
+                        >
+                            <Tab label="List" />
+                            <Tab label="Map" />
+                        </Tabs>
+                    </Paper>
+                </TabsWrapper>
+                    {/* <div>Book maps{" "}
+                        <span role="img" aria-label="books">
+                            📚
+                        </span>
+                        <span role="img" aria-label="map">
+                            🗺
+                        </span>
+                    </div> */}
+                    <BooksWrapper value={this.state.value}><BookList /></BooksWrapper>
+                    <MapWrapper value={this.state.value}><Mapview /></MapWrapper>
+                </LandingPageWrapper>
             </div>
-            <Mapview />
-        </div>
-     );
+         );
+    }
 }
  
 export default LandingPage;
