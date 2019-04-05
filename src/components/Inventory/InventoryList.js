b4083fd6d2b0ced497c5b0a7c6325f26f9a75986
Zooheck <davidscottflack@gmail.com>
2019-04-05 11:47:15 -0500

    Fix adding new inventory bug

--- src/components/Inventory/InventoryList.js
+++ src/components/Inventory/InventoryList.js
@@ -7,7 +7,6 @@ class Inventory extends Component {
   constructor() {
     super();
     this.state = {
-      inventory: [],
       searchText: ""
     };
   }
@@ -19,10 +18,10 @@ class Inventory extends Component {
   };
   searchBooks = () => {
     if (this.state.searchText.length === 0) {
-      return this.state.inventory;
+      return this.props.inventory;
     } else if (this.state.searchText.length > 0) {
       const searchRegex = new RegExp(this.state.searchText, "gi");
-      return this.state.inventory.filter(
+      return this.props.inventory.filter(
         book => book.title.match(searchRegex) || book.authors.match(searchRegex)
       );
     }
@@ -31,45 +30,43 @@ class Inventory extends Component {
     let userId = localStorage.getItem("userId");
     this.props.params.history.push(`${userId}/inventory/${bookId}`);
   };
-  componentWillReceiveProps(newProps) {
-    if (newProps.inventory !== this.state.inventory) {
-      this.setState({
-        inventory: this.props.inventory
-      });
-    }
-  }
   componentDidMount() {
     const userId = localStorage.getItem("userId");
     this.props.getAllInventory(userId);
   }
   render() {
-    return (
-      <div>
-        <h1>Inventory</h1>
-        <input
-          placeholder="Search inventory"
-          name="searchText"
-          value={this.state.searchText}
-          onChange={this.handleChange}
-        />
+    if (this.props.addingBook || this.props.loadingInventory) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
         <div>
-          {this.searchBooks().map(book => {
-            return (
-              <InventoryDetails
-                book={book}
-                viewBook={this.viewBook}
-                key={book.bookId}
-              />
-            );
-          })}
+          <h1>Inventory</h1>
+          <input
+            placeholder="Search inventory"
+            name="searchText"
+            value={this.state.searchText}
+            onChange={this.handleChange}
+          />
+          <div>
+            {this.searchBooks().map(book => {
+              return (
+                <InventoryDetails
+                  book={book}
+                  viewBook={this.viewBook}
+                  key={book.bookId}
+                />
+              );
+            })}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
 const mapStateToProps = state => ({
   loadingInventory: state.inventoryReducer.loadingInventory,
+  addingBook: state.bookReducer.fetchingBooks,
   inventory: state.inventoryReducer.inventory
 });
 export default connect(
