ff0308696a16b33250e038cb20dcaf5699c8f358
Zooheck <davidscottflack@gmail.com>
2019-04-10 12:23:20 -0500

    Add toast notification system. Implement notification upon deleting a checkout request.

--- src/components/AddBook/BookSearch.js
+++ src/components/AddBook/BookSearch.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import axios from "axios";
 import backendBaseUrl from "../../url";
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import SearchedBookTile from "./SearchedBookTile";
 import AddBook from "./AddBook";
 import Auth from "../Auth/Auth";
@@ -12,7 +12,7 @@ import {
   InventoryContainer,
   CardContainer
 } from "../Styles/InventoryStyles.js";
-import goodreadsLogo from "../../images/goodreads-logo.png"
+import goodreadsLogo from "../../images/goodreads-logo.png";
 class BookSearch extends Component {
   state = {
     searchText: "",
@@ -76,26 +76,53 @@ class BookSearch extends Component {
       return (
         <div>
           <InventoryContainer>
-            <Paper style={{width: "400px", margin: "20px auto", padding: "2px 4px", display: "flex", alignItems: "center"}}>
-              <InputBase 
+            <Paper
+              style={{
+                width: "400px",
+                margin: "20px auto",
+                padding: "2px 4px",
+                display: "flex",
+                alignItems: "center"
+              }}
+            >
+              <InputBase
                 placeholder="Enter the title or author of the book you own"
                 type="text"
                 name="searchText"
                 value={this.state.searchText}
-                onChange={this.handleChange} 
-                style={{marginLeft: "8px", flex: "1"}}
+                onChange={this.handleChange}
+                style={{ marginLeft: "8px", flex: "1" }}
               />
-              <IconButton aria-label="Search" style={{padding: "10px"}}>
-                  <ArrowForwardIcon />
+              <IconButton aria-label="Search" style={{ padding: "10px" }}>
+                <ArrowForwardIcon />
               </IconButton>
             </Paper>
-            <div style={{fontSize: "14px", display: "flex", justifyContent: "center", alignItems: "center", marginBottom: "20px"}}><span>Powered by</span><img src={goodreadsLogo} alt="goodreads logo" style={{height: "14px", width: "auto", paddingLeft: "5px"}}/></div>
+            <div
+              style={{
+                fontSize: "14px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                marginBottom: "20px"
+              }}
+            >
+              <span>Powered by</span>
+              <img
+                src={goodreadsLogo}
+                alt="goodreads logo"
+                style={{ height: "14px", width: "auto", paddingLeft: "5px" }}
+              />
+            </div>
             <CardContainer>
               {this.firstTenBooks().map((book, id) => {
                 return (
-                    <SearchedBookTile key={id} book={book} addBook={this.addBook} />
-                    );
-                  })}
+                  <SearchedBookTile
+                    key={id}
+                    book={book}
+                    addBook={this.addBook}
+                  />
+                );
+              })}
             </CardContainer>
           </InventoryContainer>
         </div>
