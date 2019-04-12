11fbd5d52bf0e6c64eadea65ece1bf2bdc17da47
Scott Vojik <scott.vojik@gmail.com>
2019-04-12 11:56:17 -0700

    transactions rendering

---
+++ src/components/Styles/UserProfileStyles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const ProfileWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+  padding: 10px;
+  margin: 5vw auto 0;
+
+  h1,
+  p {
+    padding: 5px;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+`;
