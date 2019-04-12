6e0ee562bb19a2971ffccdf6b36506269141b43b
Lambda_School_Loaner_98 <lambda_school_loaner_98@Lambda-School-Loaner-98s-MacBook-Pro.local>
2019-04-12 16:00:03 -0400

    inventory card style update with border radius

--- src/components/Styles/InventoryStyles.js
+++ src/components/Styles/InventoryStyles.js
@@ -39,6 +39,7 @@ export const NoBooks = styled.h2`
 
 export const BookDetailsWrapper = styled.div`
   width: 45%;
+  border-radius:7px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   display: flex;
