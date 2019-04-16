3d1a1db6369af8528ae5f433fd7bbc8df393042e
JacobLayton <jlayjlay3@gmail.com>
2019-04-16 15:53:19 -0700

    Adjusted Map Container to match map mapview css to fix box shadow

--- src/components/Styles/LandingPageStyles.js
+++ src/components/Styles/LandingPageStyles.js
@@ -124,7 +124,7 @@ export const MapWrapper = styled.div`
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
     position: static;
     width: 90%;
-    height: 100vh;
+    // height: 100vh;
     display: ${props => (props.value === 0 ? "none" : "block")};
   }
 `;
